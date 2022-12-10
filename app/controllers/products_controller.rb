b94b00d44e9d036d2bade9cf545416978189ef77
satoshitodaka <satoshitodaka0705@gmail.com>
2022-12-10 23:05:09 +0900

    add Productの基本的なCRUD機能

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,25 @@
+class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      redirect_to products_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:code, :name, :price)
+  end
+end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ApplicationRecord
-  validates :code, presence: true, uniquness: true
+  validates :code, presence: true, uniqueness: true
   validates :name, presence: true
   validates :price, presence: true, numericality: { only_integer: true, greater_than: 0 }
 end
